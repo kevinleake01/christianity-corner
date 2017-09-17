f602540587a5390259c4c8cf14faf0d1396c9f4d
Kevin Leake <kevinleake01@gmail.com>
2017-09-17 08:27:13 +0000

    Update setup-linux-kjv.sh

--- 01-kjvbible/setup-linux-kjv.sh
+++ 01-kjvbible/setup-linux-kjv.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir 01-kjvbible
+cd 01-kjvbible
 wget http://www.bibleprotector.com/KJB-PCE.pdf
 wget http://www.bibleprotector.com/KJB-PCE-PLAIN.pdf
 wget http://www.bibleprotector.com/KJB-PCE-MINION.pdf
@@ -9,4 +11,5 @@ wget http://www.bibleprotector.com/TEXT-PCE-127.txt
 wget http://www.bibleprotector.com/TEXT-PCE-127-TAB.txt
 wget http://www.Jesus-is-Lord.com/KJB-PCE13.pdf
 wget http://www.Jesus-is-Lord.com/KJB-PCE18.pdf
+cd ../
 
