9a361cbeb216d2cf91622994cf7af5233b7939cf
Kevin Leake <kevinleake01@gmail.com>
2018-06-03 20:44:16 +0000

    Update setup-linux-readingplans.sh

--- 03-readingplans/setup-linux-readingplans.sh
+++ 03-readingplans/setup-linux-readingplans.sh
@@ -7,17 +7,12 @@ wget http://www.edginet.org/mcheyne/year_classic_single_letter.pdf -O mcheyne_cl
 wget http://www.edginet.org/mcheyne/year_carson_a4.pdf -O mcheyne_carson_a4.pdf
 wget http://www.edginet.org/mcheyne/year_carson_letter.pdf -O mcheyne_carson_letter.pdf
 wget http://www.edginet.org/mcheyne/mcheyne_bookmarks.pdf
-wget http://hippocampusextensions.com/mcheyneplan/index.html -O mcheyne_hippocampus.html
 wget http://www.mcheyne.info/calendar.pdf -O mcheyne_haslam_calendar.pdf
-wget http://www.pesdirect.com/calendar.pdf -O mcheyne_calendar.pdf
-wget http://www.chapellibrary.org/pdf-english/mdbr.pdf -O mcheyne_mountzion.pdf
-wget http://www.oakleys.org.uk/system/files/mcCheyne.pdf -O mcheyne_oakleys.pdf
-wget http://www.mountcalvarybaptist.org/extra/documents/McCheyne%20Bible%20Reading.pdf -O mcheyne_mountcalvarybaptist.pdf
+wget http://www.chapellibrary.org/files/9713/7643/3302/mdbr.pdf -O mcheyne_mountzion.pdf
+wget https://www.oakleys.org.uk/files/blog_files/old/mcCheyne.pdf -O mcheyne_oakleys.pdf
 wget http://www.wholesomewords.org/family/bibleread/sword.pdf -O mcheyne_wholesomewords.pdf
 wget http://higginslakebaptistchurch.com/wp-content/uploads/2008/04/year_classic_letter.pdf -O mcheyne_higginslake.pdf
-wget http://static.crossway.org/excerpts/1581348150.1.pdf -O mcheyne_1581348150.1.pdf
 wget http://mp3.biblebase.com/workmanworkshops/MCheyne.pdf -O mcheyne_biblebase.pdf
-wget http://www.esv.org/assets/pdfs/rp.one.year.tract.pdf -O ESV_oneyeartract.pdf
 wget http://www.trinitarianbiblesociety.org/site/articles/bwl.pdf -O mcheyne_tbs.pdf
 cd ../
 
