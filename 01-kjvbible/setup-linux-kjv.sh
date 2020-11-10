bd49b74cd27b5f4823eb7c4e0dc0f404f0fdd69a
Kevin Leake <kevinleake01@gmail.com>
2020-11-10 12:44:04 +0000

    Update setup-linux-kjv.sh

--- 01-kjvbible/setup-linux-kjv.sh
+++ 01-kjvbible/setup-linux-kjv.sh
@@ -2,29 +2,34 @@
 
 mkdir 01-kjv-pce
 cd 01-kjv-pce
-wget http://www.bibleprotector.com/KJB-PCE.pdf
-wget http://www.bibleprotector.com/KJB-PCE-PLAIN.pdf
-wget http://www.bibleprotector.com/KJB-PCE-MINION.pdf
-wget http://www.bibleprotector.com/WHARTON_PCE.txt
-wget http://www.bibleprotector.com/TEXT-PCE.zip
-wget http://www.bibleprotector.com/TEXT-PCE-127.txt
-wget http://www.bibleprotector.com/TEXT-PCE-127-TAB.txt
 wget http://www.Jesus-is-Lord.com/KJB-PCE13.pdf
 wget http://www.Jesus-is-Lord.com/KJB-PCE18.pdf
 wget https://www.jesus-is-savior.com/Bible/KJB/pce.doc -O jesus-is-savior-pce.doc
 wget https://www.jesus-is-savior.com/Bible/KJB/pce.pdf -O jesus-is-savior-pce.pdf
 wget https://www.jesus-is-savior.com/Bible/KJB/pce_minion.pdf -O jesus-is-savior-pce-minion.pdf
 wget http://www.studylamp.com/downloads/KJV.zip -O studylamp-kjv.zip
-wget http://uploads.worldlibrary.org/uploads/pdf/20151027053828king_james_bible_pure_cambridge_edition_1_2095.pdf
+wget https://www.crosswire.org/ftpmirror/pub/sword/packages/rawzip/KJVPCE.zip
 git clone git://github.com/webplantmedia/the-holy-bible.git 0001-webplantmedia-theholybible
 git clone git://github.com/psyclone241/kjv-pce-api.git 0002-psyclone241-kjv-pce-api
 git clone git://github.com/tomhoover/hisword.net.git 0003-tomhoover-hisword
+git clone git://github.com/R4wm/sqlite3_kjv.git 0004-r4wm-sqlite3-kjv
+git clone git://github.com/anoxic/bibleqe.git 0005-anoxic-bibleqe
 cd ../
 
 mkdir 02-other-kjv
 cd 02-other-kjv
 wget http://www.gasl.org/refbib/Bible_King_James_Version.pdf
-wget http://www.av-1611.com/KJBIBLE.pdf
+wget http://www.davince.com/download/kjvbible.pdf
+wget http://www.davince.com/download/kjvbiblea.pdf
+wget https://ebible.org/pdf/eng-kjv2006/eng-kjv2006_all.pdf
+wget https://ebible.org/pdf/eng-kjv2006/eng-kjv2006_prt.pdf
+wget https://ebible.org/pdf/eng-kjv2006/eng-kjv2006_book.pdf
+wget https://ebible.org/pdf/eng-kjv/eng-kjv_all.pdf
+wget https://ebible.org/pdf/eng-kjv/eng-kjv_prt.pdf
+wget https://ebible.org/pdf/eng-kjv/eng-kjv_book.pdf
+wget https://ebible.org/pdf/engkjvcpb/engkjvcpb_all.pdf
+wget https://ebible.org/pdf/engkjvcpb/engkjvcpb_prt.pdf
+wget https://ebible.org/pdf/engkjvcpb/engkjvcpb_book.pdf
 git clone git://github.com/aruljohn/Bible-kjv.git 0001-aruljohn-bible-kjv
 git clone git://github.com/jjuliano/kjv-bible-in-html5.git 0002-jjuliano-kjv-in-html5
 git clone git://github.com/robertrouse/KJV-bible-database-with-metadata-MetaV-.git 0003-robertrouse-kjv-database-with-metadata-v
